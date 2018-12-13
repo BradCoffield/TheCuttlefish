450e09d6eaab8e733c867bfcca0d52977d05be2a
Brad Coffield <bcoffield@gmail.com>
2018-12-13 11:12:50 -0700

    setup with local env variables

---
+++ src/Firebase.js
@@ -0,0 +1,27 @@
+ /* eslint-disable */ 
+import * as firebase from 'firebase';
+import firestore from 'firebase/firestore'
+
+const settings = {timestampsInSnapshots: true};
+
+const config = {
+    apiKey: process.env.VUE_APP_FIRESTORE_API_KEY,
+    authDomain: process.env.VUE_APP_FIRESTORE_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_FIRESTORE_DATABASE_URL,
+    projectId: process.env.VUE_APP_FIRESTORE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIRESTORE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIRESTORE_MESSAGING_SENDER_ID
+};
+// const config = {
+//     apiKey: process.env.VUE_APP_FIRESTORE_API_KEY,
+//     authDomain: "vue-crud-tutorial.firebaseapp.com",
+//     databaseURL: "https://vue-crud-tutorial.firebaseio.com",
+//     projectId: "vue-crud-tutorial",
+//     storageBucket: "vue-crud-tutorial.appspot.com",
+//     messagingSenderId: "682064666020"
+// };
+firebase.initializeApp(config);
+
+firebase.firestore().settings(settings);
+
+export default firebase;

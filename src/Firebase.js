c8520e4c0d710d19e0ee5a04fdaef65878c7e896
Brad Coffield <bcoffield@gmail.com>
2018-12-28 09:42:04 -0500

    yep

--- src/Firebase.js
+++ src/Firebase.js
@@ -12,16 +12,11 @@ const config = {
     storageBucket: process.env.VUE_APP_FIRESTORE_STORAGE_BUCKET,
     messagingSenderId: process.env.VUE_APP_FIRESTORE_MESSAGING_SENDER_ID
 };
-// const config = {
-//     apiKey: process.env.VUE_APP_FIRESTORE_API_KEY,
-//     authDomain: "vue-crud-tutorial.firebaseapp.com",
-//     databaseURL: "https://vue-crud-tutorial.firebaseio.com",
-//     projectId: "vue-crud-tutorial",
-//     storageBucket: "vue-crud-tutorial.appspot.com",
-//     messagingSenderId: "682064666020"
-// };
+ 
 firebase.initializeApp(config);
 
 firebase.firestore().settings(settings);
 
 export default firebase;
+
+ 

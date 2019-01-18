2d86baf19ff10961239648813cb5043cd66fd7d5
Brad Coffield <bcoffield@gmail.com>
2019-01-18 10:34:36 -0700

    sending to firestore and deleting locally !!

--- src/views/ManageContentTypes.vue
+++ src/views/ManageContentTypes.vue
@@ -11,39 +11,47 @@
         :key="index"
         style="display:inline-block;"
       >
-        <div v-if="ct.edit" class="columns ">
+        <div v-if="ct.edit" class="columns">
           <input
-            class="input  "
+            class="input"
+            v-model="ct.name"
+             
+            v-focus
+          >
+        <!-- <div v-if="ct.edit" class="columns">
+          <input
+            class="input"
             v-model="ct.name"
             @blur="ct.edit = false;  "
             @keyup.enter="ct.edit=false;  "
             v-focus
-          >
-          <button class="button is-success   " @click="ct.edit=false" style="margin-left:5px;">
+          > -->
+          <button class="button is-danger" @click="deleteThisOne(ct.name)">Delete</button>
+          <button class="button is-success" @click="ct.edit=false" style="margin-left:5px;">
             <span class="mdi mdi-check"></span>
           </button>
         </div>
 
         <div v-else>
-          <button class="button is-light  " @click="ct.edit = true;">{{ct.name}}</button>
+          <button class="button is-light" @click="ct.edit = true;">{{ct.name}}</button>
         </div>
       </li>
-      <!-- TODO: create an add to the list button. click creates input on save pushes to the fromfirestoreobjectified -->
+     
       <li style="display:inline-block;">
-        <button class="button is-white" @click="pushNewEmpty"  >
+        <button class="button is-white" @click="pushNewEmpty">
           <span class="mdi mdi-plus"></span>
         </button>
       </li>
     </ul>
-   
+
     <div class="form-buttons">
-        <button @click="sendUpdate" class="button is-info">
-          <span class="mdi mdi-check"></span> Update
-        </button>
-        <button @click="goHome" class="button is-danger">
-          <span class="mdi mdi-cancel"></span> Cancel
-        </button>
-      </div>
+      <button @click="sendUpdate" class="button is-info">
+        <span class="mdi mdi-check"></span> Update
+      </button>
+      <button @click="goHome" class="button is-danger">
+        <span class="mdi mdi-cancel"></span> Cancel
+      </button>
+    </div>
   </div>
 </template>
 
@@ -59,10 +67,11 @@ export default {
         .firestore()
         .collection("content-types")
         .doc("forDatabases"),
+      ref2: firebase.firestore().collection("content-types"),
       fromFirestore: [],
       fromFirestoreObjectified: [],
       readyForFirestore: [],
-       
+
       editButtonEditActive: false
     };
   },
@@ -80,40 +89,57 @@ export default {
         alert("No such document!");
       }
     });
+    this.ref2.get().then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+      });
+    });
   },
   methods: {
+    deleteThisOne(thang){
+      
+      this.fromFirestoreObjectified = this.fromFirestoreObjectified.filter(item => item.name != thang);
+    },
+    filterEmpties() {
+      this.fromFirestoreObjectified = this.fromFirestoreObjectified.filter(
+        item => item.name != ""
+      );
+    },
     prepCTforUpdate() {
-      this.fromFirestoreObjectified = this.fromFirestoreObjectified.filter(item => item.name != "");
-
+      this.readyForFirestore = [];
+      this.filterEmpties();
       //loop over the objectified array and get all the name values.
       this.fromFirestoreObjectified.forEach(i => {
-      
-        this.readyForFirestore.push(i.name); 
+        this.readyForFirestore.push(i.name);
       });
     },
-    pushNewEmpty(){
-      this.fromFirestoreObjectified.push({name: "", edit: true})
+    pushNewEmpty() {
+      this.fromFirestoreObjectified.push({ name: "", edit: true });
     },
-     sendUpdate(evt) {
+    sendUpdate(evt) {
       this.prepCTforUpdate();
-    
 
       evt.preventDefault();
-     this.ref.set(this.readyForFirestore);
-    //  this.ref.set(this.readyForFirestore, { merge: true });
+      this.ref
+        .set({ forDatabases: this.readyForFirestore }, { merge: true })
+        .catch(function(error) {
+          console.error("Error writing document: ", error);
+        });
+
+      //  this.ref.set(this.readyForFirestore, { merge: true });
 
       this.$toast.open({
-        message: "Database submitted!",
+        message: "Content Types Updated!",
         type: "is-success"
       });
-      this.initialData();
-      this.$forceUpdate();
+
       document.body.scrollTop = 0; // For Safari
       document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     },
     goHome() {
       router.push("/");
-    },
+    }
   },
   directives: {
     focus: {

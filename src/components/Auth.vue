44779057824214932211bb04157cb873e91b2f27
Brad Coffield <bcoffield@gmail.com>
2019-02-28 15:24:25 -0700

    these come from that tutorial but things aren't working right

---
+++ src/components/Auth.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div id="firebaseui-auth-container"></div>
+</template>
+
+<script>
+import firebase from "../Firebase";
+import router from "../router";
+import * as firebaseui from 'firebaseui'
+
+export default {
+  name: 'auth',
+  mounted() {
+    var uiConfig = {
+      signInSuccessUrl: '/success',
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        ]
+      };
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start('#firebaseui-auth-container', uiConfig);
+    },
+}
+</script>
